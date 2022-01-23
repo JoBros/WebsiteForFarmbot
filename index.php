5ff8f62504faa353b4e5c55cc775f066faa1aafb
ChrisK <Christian.Kuellmer@netdate-lab.de>
2022-01-23 18:19:19 +0100

    Update for coupling

--- index.php
+++ index.php
@@ -47,73 +47,7 @@
 
 </div>
 <script>
-  // load current chart package
-  google.charts.load("current", {
-    packages: ["corechart", "line"]
-  });
-  // set callback function when api loaded
-  google.charts.setOnLoadCallback(drawChart);
 
-  function drawChart() {
-    // create data object with default value
-    let data = google.visualization.arrayToDataTable([
-      //    ["Datenreihe1" , "Feuchtigkeit [in %]?"],
-      //      [1,0],
-      //      [4,2],
-      //      [2,1],
-      <?php
-      //-> Read from Database
-      $servername = "192.168.100.49";
-      $username = "me";
-      $password = "Alzheimer";
-      $dbname = "Farmbot";
-
-      // Create connection
-      $conn = new mysqli($servername, $username, $password, $dbname);
-      // Check connection
-      if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-      }
-
-      $sql = "SELECT sensor, value, created_at FROM SensorDat t where sensor='WS_1' ORDER BY created_at asc limit 288";
-      $result = $conn->query($sql);
-
-      echo "['Datum', 'Wassersensor'],";
-
-      if ($result->num_rows > 0) {
-        // output data of each row
-        $datumszahl = $result->num_rows;
-        while($row = $result->fetch_assoc()) {
-          echo "[ $created_at," . $row["value"] . "]";
-          if(0 < ($datumszahl) - 1){
-            echo ",";
-          }
-          $datumszahl = $datumszahl -1;
-        }
-      } else {
-        echo "0 results";
-      }
-      $conn->close();
-
-      ?>
-    ] );
-    // create options object with titles, colors, etc.
-    let options = {
-
-      curveType: 'function',
-      hAxis: {
-        title: "Zeit"
-      },
-      vAxis: {
-        title: "Wert"
-      }
-    };
-    // draw chart on load
-    let chart = new google.visualization.LineChart(
-      document.getElementById("chart_div")
-    );
-    chart.draw(data, options);
-  }
 </script>
 
 <script src="js/main.js"></script>
