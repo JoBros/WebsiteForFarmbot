4a1227b4ed8452e3fffab5660261adccbc1cd858
ChrisK <Christian.Kuellmer@netdate-lab.de>
2022-01-23 17:36:28 +0100

    Änderung der Darstellung

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
         die("Connection failed: " . $conn->connect_error);
       }
 
-      $sql = "SELECT sensor, value, created_at FROM SensorDat t ORDER BY created_at asc limit 288";
+      $sql = "SELECT sensor, value, created_at FROM SensorDat t ORDER BY created_at desc limit 288";
       $result = $conn->query($sql);
 
       echo "['Datum', 'Wassersensor'],";
