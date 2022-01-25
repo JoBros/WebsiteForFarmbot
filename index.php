ee1f0019a80cf024f8cf36007ef1420343273e5c
ChrisK <Christian.Kuellmer@netdate-lab.de>
2022-01-25 19:52:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -34,18 +34,18 @@
 <div style="padding-left:16px">
   <h2>Wassersensorik </h2>
 
-  <div style="min-width:30%; min-height: 30%" id="chart_div"></div>
+  <div style="min-width:30%; min-height: 30%" id="chart_div0"></div>
 
   <p>Hier wird der aktuelle Wasserwert angezeigt, wobei rechts der aktuelle Wasserwert angezeigt wird, und über den Graphen der Trend angenommen werden kann. </p>
   <h3>Der aktuelle Bodenwasserwert liegt bei <Label id="bwW"></Label></h3>
 
   <h2>Temperatursensorik</h2>
 
-  <div style="min-width:30%; min-height: 30%" id="chart_div2"></div>
+  <div style="min-width:30%; min-height: 30%" id="chart_div1"></div>
 
   <h2>Welche arbeiten wurden in der letzten Zeit gemacht?</h2>
 
-  <div style="min-width:30%; min-height: 30%" id="chart_div3"></div>
+  <div style="min-width:30%; min-height: 30%" id="chart_div2"></div>
 
 </div>
 <script>
@@ -120,17 +120,15 @@
     );
     chart.draw(data, options);
   }
-</script>
 
-<script>
-  // load current chart package
+  //zweites Diagramm
   google.charts.load("current", {
     packages: ["corechart", "line"]
   });
   // set callback function when api loaded
-  google.charts.setOnLoadCallback(drawChart);
+  google.charts.setOnLoadCallback(drawChart2);
 
-  function drawChart() {
+  function drawChart2() {
     // create data object with default value
     let data = google.visualization.arrayToDataTable([
       //    ["Datenreihe1" , "Feuchtigkeit [in %]?"],
@@ -151,7 +149,7 @@
         die("Connection failed: " . $conn->connect_error);
       }
 
-      $sql = "SELECT value, created_at FROM SensorDat t where sensor='TP_1' ORDER BY created_at desc limit 288";
+      $sql = "SELECT value, created_at FROM SensorDat t where sensor='WS_1' ORDER BY created_at desc limit 288";
       $result = $conn->query($sql);
 
       echo "['Datum', 'Wassersensor'],";
@@ -171,8 +169,6 @@
       } else {
         echo "0 results";
       }
-
-
       $conn->close();
 
       ?>
@@ -198,79 +194,9 @@
   }
 </script>
 
-<script>
-  // load current chart package
-  google.charts.load("current", {
-    packages: ["corechart", "line"]
-  });
-  // set callback function when api loaded
-  google.charts.setOnLoadCallback(drawChart);
 
-  function drawChart() {
-    // create data object with default value
-    let data = google.visualization.arrayToDataTable([
-      //    ["Datenreihe1" , "Feuchtigkeit [in %]?"],
-      //      [1,0],
-      //      [4,2],
-      //      [2,1],
-      <?php
-      //-> Read from Database
-      $servername = "192.168.100.49";
-      $username = "me";
-      $password = "Alzheimer";
-      $dbname = "Farmbot";
 
-      // Create connection
-      $conn = new mysqli($servername, $username, $password, $dbname);
-      // Check connection
-      if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-      }
 
-      $sql = "SELECT value, created_at FROM SensorDat t where sensor='LF_1' ORDER BY created_at desc limit 288";
-      $result = $conn->query($sql);
-
-      echo "['Datum', 'Wassersensor'],";
-
-      if ($result->num_rows > 0) {
-        // output data of each row
-        $datumszahl = $result->num_rows;
-        while($row = $result->fetch_assoc()) {
-          $datumzeit=$row["created_at"];
-          $dt = strtotime($datumzeit);
-          echo "[ " . $dt . " ," . $row["value"] . "]";
-          if(0 < ($datumszahl) - 1){
-            echo ",";
-          }
-          $datumszahl = $datumszahl -1;
-        }
-      } else {
-        echo "0 results";
-      }
-      $conn->close();
-
-      ?>
-    ] );
-    // create options object with titles, colors, etc.
-    let options = {
-
-      curveType: 'function',
-      hAxis: {
-        textPosition: 'none',
-        title: "Zeit"
-      },
-      vAxis: {
-        title: "Wert",
-        minValue: 0
-      }
-    };
-    // draw chart on load
-    let chart = new google.visualization.LineChart(
-      document.getElementById("chart_div3")
-    );
-    chart.draw(data, options);
-  }
-</script>
 
 
 
