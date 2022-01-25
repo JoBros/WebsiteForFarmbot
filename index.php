5d3a6f7579616961dd8a9b1aca893c9e65e883ef
ChrisK <Christian.Kuellmer@netdate-lab.de>
2022-01-25 19:22:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -121,6 +121,156 @@
   }
 </script>
 
+<script>
+  // load current chart package
+  google.charts.load("current", {
+    packages: ["corechart", "line"]
+  });
+  // set callback function when api loaded
+  google.charts.setOnLoadCallback(drawChart);
+
+  function drawChart() {
+    // create data object with default value
+    let data = google.visualization.arrayToDataTable([
+      //    ["Datenreihe1" , "Feuchtigkeit [in %]?"],
+      //      [1,0],
+      //      [4,2],
+      //      [2,1],
+      <?php
+      //-> Read from Database
+      $servername = "192.168.100.49";
+      $username = "me";
+      $password = "Alzheimer";
+      $dbname = "Farmbot";
+
+      // Create connection
+      $conn = new mysqli($servername, $username, $password, $dbname);
+      // Check connection
+      if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+
+      $sql = "SELECT value, created_at FROM SensorDat t where sensor='TP_1' ORDER BY created_at desc limit 288";
+      $result = $conn->query($sql);
+
+      echo "['Datum', 'Wassersensor'],";
+
+      if ($result->num_rows > 0) {
+        // output data of each row
+        $datumszahl = $result->num_rows;
+        while($row = $result->fetch_assoc()) {
+          $datumzeit=$row["created_at"];
+          $dt = strtotime($datumzeit);
+          echo "[ " . $dt . " ," . $row["value"] . "]";
+          if(0 < ($datumszahl) - 1){
+            echo ",";
+          }
+          $datumszahl = $datumszahl -1;
+        }
+      } else {
+        echo "0 results";
+      }
+      $conn->close();
+
+      ?>
+    ] );
+    // create options object with titles, colors, etc.
+    let options = {
+
+      curveType: 'function',
+      hAxis: {
+        textPosition: 'none',
+        title: "Zeit"
+      },
+      vAxis: {
+        title: "Wert",
+        minValue: 0
+      }
+    };
+    // draw chart on load
+    let chart = new google.visualization.LineChart(
+      document.getElementById("chart_div2")
+    );
+    chart.draw(data, options);
+  }
+</script>
+
+<script>
+  // load current chart package
+  google.charts.load("current", {
+    packages: ["corechart", "line"]
+  });
+  // set callback function when api loaded
+  google.charts.setOnLoadCallback(drawChart);
+
+  function drawChart() {
+    // create data object with default value
+    let data = google.visualization.arrayToDataTable([
+      //    ["Datenreihe1" , "Feuchtigkeit [in %]?"],
+      //      [1,0],
+      //      [4,2],
+      //      [2,1],
+      <?php
+      //-> Read from Database
+      $servername = "192.168.100.49";
+      $username = "me";
+      $password = "Alzheimer";
+      $dbname = "Farmbot";
+
+      // Create connection
+      $conn = new mysqli($servername, $username, $password, $dbname);
+      // Check connection
+      if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+
+      $sql = "SELECT value, created_at FROM SensorDat t where sensor='LF_1' ORDER BY created_at desc limit 288";
+      $result = $conn->query($sql);
+
+      echo "['Datum', 'Wassersensor'],";
+
+      if ($result->num_rows > 0) {
+        // output data of each row
+        $datumszahl = $result->num_rows;
+        while($row = $result->fetch_assoc()) {
+          $datumzeit=$row["created_at"];
+          $dt = strtotime($datumzeit);
+          echo "[ " . $dt . " ," . $row["value"] . "]";
+          if(0 < ($datumszahl) - 1){
+            echo ",";
+          }
+          $datumszahl = $datumszahl -1;
+        }
+      } else {
+        echo "0 results";
+      }
+      $conn->close();
+
+      ?>
+    ] );
+    // create options object with titles, colors, etc.
+    let options = {
+
+      curveType: 'function',
+      hAxis: {
+        textPosition: 'none',
+        title: "Zeit"
+      },
+      vAxis: {
+        title: "Wert",
+        minValue: 0
+      }
+    };
+    // draw chart on load
+    let chart = new google.visualization.LineChart(
+      document.getElementById("chart_div3")
+    );
+    chart.draw(data, options);
+  }
+</script>
+
+
+
 <script src="js/main.js"></script>
 
 </body>
