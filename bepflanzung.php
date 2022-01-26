1baad19cf035c522f735a1294f159abb730c7bc6
JoBros <JoBros@users.noreply.github.com>
2022-01-26 14:26:57 +0100

    Update bepflanzung.php

--- bepflanzung.php
+++ bepflanzung.php
@@ -30,54 +30,15 @@
 <center>
 Positionstabelle:
 
- <div class="anzeigeStatus">
-  <table style="mso-cellspacing: 20px;" class="anzeigeStatus">
-    <?php
-
-    $servername = "192.168.100.49";
-    $username = "me";
-    $password = "Alzheimer";
-    $dbname = "Farmbot";
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    }
-
-    $sql = "SELECT t.* FROM History t ORDER BY timest DESC";
-    $result = $conn->query($sql);
-
-
-    if ($result->num_rows > 0) {
-      // output data of each row
-      while ($row = $result->fetch_assoc()) {
-        echo "<tr class='anzeigeStatus'><td class='anzeigeStatus'>" . $row["x"] . "</td><td class='anzeigeStatus'>". $row["y"] . "</td><td class='anzeigeStatus'>" . $row["doing"] ."</td><td class='anzeigeStatus'>" . $row["timest"] . "</td></tr>";
-      }
-    } else {
-      echo "Kein Wert verfügbar";
-    }
-    $conn->close();
-    ?>
-  </table>
-  </div>
-
-
-
-
-
-
-
-
-
 <table hspace="300" vspace="200" border='1' bordercolordark="#800000" bordercolorlight="#FF0000" bgcolor="grey">
 <?php
+//Connection Data
 $host="192.168.100.49";
 $username="me";
 $password="Alzheimer";
 $db_name="Farmbot";
 $tbl_name="PflanzenPos";
+
 $tbl_width="6";
 $tbl_length="11";
 $connection=mysqli_connect("$host","$username","$password","$db_name");
@@ -85,20 +46,6 @@ if (mysqli_connect_errno())
 {
     echo "The application has failed to connect to the mysql database server: " .mysqli_connect_error();
 }
-$result = mysqli_query($connection, "SELECT * FROM PflanzenPos")or die("Error: " . mysqli_error($connection));
-
-$sql = "SELECT value,x FROM PflanzenPos ORDER BY x DESC";
-      $result = $connection->query($sql);
-if ($result->num_rows > 0) {
-      // output data of each row
-      while ($row = $result->fetch_assoc()) {
-        echo "<tr class='anzeigeStatus'><td class='anzeigeStatus'>" . $row["x"] . "</td><td class='anzeigeStatus'>". $row["y"] . "</td><td class='anzeigeStatus'>" . $row["doing"] ."</td><td class='anzeigeStatus'>" . $row["timest"] . "</td></tr>";
-      }
-    } else {
-      echo "Kein Wert verfügbar";
-    }
-    $conn->close();
-
 
 for($i=1; $i<=$tbl_length; $i++)
 {
@@ -109,7 +56,6 @@ for($i=1; $i<=$tbl_length; $i++)
     }
     echo "</tr>";
 }
-
 mysqli_close($connection);
 ?>
 </table>
