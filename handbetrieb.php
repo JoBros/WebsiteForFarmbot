c79777398dc5370155a3b3bef6a3040e4583765f
Eugen2586 <40714234+Eugen2586@users.noreply.github.com>
2022-01-26 23:05:26 +0100

    Update handbetrieb.php

--- handbetrieb.php
+++ handbetrieb.php
@@ -18,7 +18,8 @@
 </head>
 
 <body>
-<h1 style="background-color: #333; font-family: Arial, Helvetica, sans-serif; padding: 15px; margin: 0px; color: white">Farmbotsteuerung</h1>
+<h1 style="background-color: #333; font-family: Arial, Helvetica, sans-serif; padding: 15px; margin: 0px; color: white">
+  Farmbotsteuerung</h1>
 <div class="topnav">
   <a href="index.php">Status</a>
   <a href="bepflanzung.php">Bepflanzung</a>
@@ -28,48 +29,74 @@
 </div>
 
 <h2>Handbetriebsseite zum Farmbot</h2>
-<p>Auf dieser Seite kann alles gefunden werden, was zum manuellen Steuern des Farmbots nötig ist. Auf dieser Seite ist es möglich das Gehäuse in X, Y, und Z Richtung zu bewegen. Auch die Gießbewegung und die Wässerungsfunktion kann hier genutzt werden. Ebenso kann der Workflow zum Sähen seine Einleitung finden. </p>
-<div style="padding: inherit; margin: fill; width: 100%; height: 100%">
-<table class="rechts">
+<p>Auf dieser Seite kann alles gefunden werden, was zum manuellen Steuern des Farmbots nötig ist. Auf dieser Seite ist
+  es möglich das Gehäuse in X, Y, und Z Richtung zu bewegen. Auch die Gießbewegung und die Wässerungsfunktion kann hier
+  genutzt werden. Ebenso kann der Workflow zum Sähen seine Einleitung finden. </p>
+<table>
   <tr>
     <td class="anzeigeStatus" style="visibility: hidden"></td>
-    <td class="anzeigeStatus"><center><Button id="A" class="anzeigeStatus">x ></Button></center></td>
+    <td class="anzeigeStatus">
+      <center>
+        <Button id="A" class="anzeigeStatus">x ></Button>
+      </center>
+    </td>
     <td class="anzeigeStatus" style="visibility: hidden"></td>
   </tr>
   <tr>
-    <td class="anzeigeStatus"><center><Button id="W" class="anzeigeStatus">y <</Button></center></td>
+    <td class="anzeigeStatus">
+      <center>
+        <Button id="W" class="anzeigeStatus">y <</Button>
+      </center>
+    </td>
     <td class="anzeigeStatus" style="visibility: hidden"></td>
-    <td class="anzeigeStatus"><center><Button id="S" class="anzeigeStatus">y ></Button></center></td>
+    <td class="anzeigeStatus">
+      <center>
+        <Button id="S" class="anzeigeStatus">y ></Button>
+      </center>
+    </td>
   </tr>
   <tr>
     <td class="anzeigeStatus" style="visibility: hidden"></td>
-    <td class="anzeigeStatus"><center><Button id="D" class="anzeigeStatus">x <</Button></center></td>
+    <td class="anzeigeStatus">
+      <center>
+        <Button id="D" class="anzeigeStatus">x <</Button>
+      </center>
+    </td>
     <td class="anzeigeStatus" style="visibility: hidden"></td>
   </tr>
   <tr>
-    <td class="anzeigeStatus"><center><Button id="Q" class="anzeigeStatus">z <</Button></center></td>
+    <td class="anzeigeStatus">
+      <center>
+        <Button id="Q" class="anzeigeStatus">z <</Button>
+      </center>
+    </td>
     <td class="anzeigeStatus" style="visibility: hidden"></td>
-    <td class="anzeigeStatus"><center><Button id="E" class="anzeigeStatus">z ></Button></center></td>
+    <td class="anzeigeStatus">
+      <center>
+        <Button id="E" class="anzeigeStatus">z ></Button>
+      </center>
+    </td>
   </tr>
 </table>
-        <div class="rechts">
-          <Button id="wasserbtn" style="background-color: dodgerblue; color: white; height: 4em; width: 6em;">
-            Wasser
-          </Button>
-          <input id="_waternumber" style="height: 3em; width: 6em;" type="number" disabled="true"/><Label style="height: 4em; width: 6em;">%
-            Wasser</Label>
-          <br>
-          <Button id="hakbtn" style="height: 4em; width: 6em;">Harke</Button>
-          <input disabled="true" style="height: 3em; width: 6em;"/><Label style="height: 3em; width: 6em;" hidden="true">Harken
-            ausgeführt!</Label>
-          <br>
-          <Button id="saatbtn" style="height: 4em; width: 6em;">Saat</Button>
-          <input id="frucht" style="height: 3em; width: 6em;"/><Label style="height: 3em; width: 6em;" hidden="true">Saat
-            ausgeführt!</Label>
-        </div>
+<div>
+  <Button id="wasserbtn" style="background-color: dodgerblue; color: white; height: 4em; width: 6em;">
+    Wasser
+  </Button>
+  <input id="_waternumber" style="height: 3em; width: 6em;" type="number" disabled="true"/><Label
+    style="height: 4em; width: 6em;">%
+    Wasser</Label>
+  <br>
+  <Button id="hakbtn" style="height: 4em; width: 6em;">Harke</Button>
+  <input disabled="true" style="height: 3em; width: 6em;"/><Label style="height: 3em; width: 6em;" hidden="true">Harken
+    ausgeführt!</Label>
+  <br>
+  <Button id="saatbtn" style="height: 4em; width: 6em;">Saat</Button>
+  <input id="frucht" style="height: 3em; width: 6em;"/><Label style="height: 3em; width: 6em;" hidden="true">Saat
+    ausgeführt!</Label>
 </div>
-  <script src="js/main.js"></script>
-  <script src="js/WebsocketControl.js"></script>
+</div>
+<script src="js/main.js"></script>
+<script src="js/WebsocketControl.js"></script>
 </body>
 
 </html>
