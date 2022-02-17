e8f68b2dbc64a0485801b2b1fcc6b0e4466f449d
Eugen2586 <40714234+Eugen2586@users.noreply.github.com>
2022-02-17 11:48:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -119,7 +119,7 @@
         die("Connection failed: " . $conn->connect_error);
       }
 
-      $sql = "SELECT value, created_at FROM SensorDat t where sensor='WS_1' ORDER BY created_at desc limit 120";
+      $sql = "SELECT value, created_at FROM SensorDat t where sensor='WS_1' ORDER BY created_at desc"; //limit 120
       $result = $conn->query($sql);
 
       echo "['Datum', 'WS_1'],";
